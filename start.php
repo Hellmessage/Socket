9b7ab250012cf2cf9f8fd4a2461881e8f42430e5
Hellmessage <617494454@qq.com>
2017-04-04 13:45:28 +0800

    Up

--- start.php
+++ start.php
@@ -1,12 +1,10 @@
 <?php
 use Workerman\Worker;
 require_once __DIR__ . '/Workerman/Autoloader.php';
-
 $global_uid = 0;
 // 当客户端连上来时分配uid，并保存连接，并通知所有客户端
 function handle_connection($connection){
     global $text_worker, $global_uid;
-    // 为这个链接分配一个uid
     $connection->uid = ++$global_uid;
 }
 // 当客户端发送消息过来时，转发给所有人
@@ -34,23 +32,11 @@ function handle_message($connection, $data){
     }
   }
 }
-//当客户端断开时，广播给所有客户端
-function handle_close($connection){
-    global $text_worker;
-    foreach($text_worker->connections as $conn){
-      $lid = $connection->lid;
-      if($conn->lid == $lid){
-        $conn->send(json_encode(array("type"=>3),array("uid"=>$connection->uid)));
-      }
-    }
-}
-
 // 创建一个文本协议的Worker监听2347接口
 $text_worker = new Worker("tcp://0.0.0.0:2347");
 // 只启动1个进程，这样方便客户端之间传输数据
 $text_worker->count = 1;
 $text_worker->onConnect = 'handle_connection';
 $text_worker->onMessage = 'handle_message';
-$text_worker->onClose = 'handle_close';
 Worker::runAll();
 ?>
